bc041f77a457b94b433d206744f93150cd018f57
yoshimi <yoshi3923@icloud.com>
2022-07-31 01:30:42 +0900

    delete comment code
    delete code

--- echo.py
+++ echo.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# echo.py: sample script to print ros message to terminal
-# Author: Ravi Joshi
-# Date: 2020/03/02
+# echo.py: 
+# Author:
+# Date: 2022/07/031
 
-# import modules
 import rospy
 from ros_openpose.msg import Frame
 import cv2
@@ -18,11 +17,10 @@ import numpy as np
 class depth_estimater:
     queue_size = 100
     fps = 100.
-    delay = 0.5#1 / fps * 0.5
+    delay = 0.5
 
     def __init__(self):
         rospy.init_node('echo', anonymous=False)
-        #rospy.init_node('depth_estimater', anonymous=True)
         self.bridge = CvBridge()
         frame_topic = rospy.get_param('~pub_topic')
         #sub_rgb = message_filters.Subscriber("/camera/color/image_raw",Image)
@@ -38,15 +36,14 @@ class depth_estimater:
             depth_image = self.bridge.imgmsg_to_cv2(depth_data, '32FC1')#32FC1
             depth_image = cv2.resize(depth_image, (int(depth_image.shape[1]*1), int(depth_image.shape[0]*1)))
             text = [bodyPart.pixel for person in pose_data.persons for bodyPart in person.bodyParts]
-            #print(depth_image.shape[0])
         except CvBridgeError as e:
             rospy.logerr(e)
 
         textx = int(text[4].x)
         texty = int(text[4].y)
-        #print(textx, texty)
+        #print(textx, texty) print pixel(x, y)
         sum = 0.0
-        #if depth_image.item(texty, textx) != 0: #ex.３次元座標とってみる
+
         if (textx != 0) and (texty != 0): 
             sum += depth_image.item(texty, textx)
         ave = sum*0.001
@@ -54,15 +51,15 @@ class depth_estimater:
         y3 = texty
         point = np.array([x3,y3,ave])
         if ave==0 :
-            print("ERROR: cannot get depth data ! ")
+            print("ERROR: cannot get data ! ")
             return 
 
         xx=point[0]
         yy=point[1]
         zz=point[2]
         
-        xx =float((xx-334.005)/618.136)*zz
-        yy =float((yy-231.885)/618.370)*zz
+        xx =float((xx-334.005)/618.136)*zz #パラメータは各自のカメラで変えて下さい
+        yy =float((yy-231.885)/618.370)*zz #パラメータは各自のカメラで変えて下さい
         xx = '{:.3f}'.format(xx)
         yy = '{:.3f}'.format(yy)
 
@@ -77,7 +74,6 @@ class depth_estimater:
         ### =======================
 
 if __name__ == '__main__':
-    #main()
     try:
         de = depth_estimater()
         rospy.spin()
